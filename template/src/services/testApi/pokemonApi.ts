ca4ad7f5ef4276613a62fe8d2c72c753036d2337
Balthazar33 <castielcollins57@yahoo.in>
2024-01-08 13:46:08 +0530

    nit: gitignore updated, comments updated

--- template/src/services/testApi/pokemonApi.ts
+++ template/src/services/testApi/pokemonApi.ts
@@ -7,21 +7,21 @@ export const pokemonApi = createApi({
   reducerPath: 'pokeApi',
   baseQuery: fetchBaseQuery({baseUrl: BASE_URL}),
   endpoints: builder => ({
-    // mutation
+    // Get all pokemons
     getAllPokemon: builder.mutation({
       query: () => ({
         url: Endpoints.ALL,
         method: FetchMethods.GET,
       }),
     }),
-    // get pokemon details by name
+    // Get pokemon details by name
     getDetailsByName: builder.mutation({
       query: ({name}) => ({
         url: `${Endpoints.ALL}${name}`,
         method: FetchMethods.GET,
       }),
     }),
-    // the endpoint for the following POST request does not exist and is added merely as an example
+    // The endpoint for the following POST request does not exist and is added merely as an example
     addPokemonByName: builder.mutation({
       query: body => ({
         url: Endpoints.ADDNEW,

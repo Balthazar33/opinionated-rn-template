73fe320d543d41b0fac64f1f7a79b3467af35de6
Balthazar33 <castielcollins57@yahoo.in>
2024-03-27 10:47:49 +0530

    docs: added error boundary details

--- template/src/components/index.ts
+++ template/src/components/index.ts
@@ -1,4 +1,5 @@
 import {Loader} from './Loader';
 import {NoInternet} from './NoInternet';
+import {ErrorFallback} from './ErrorFallback';
 
-export {Loader, NoInternet};
+export {Loader, NoInternet, ErrorFallback};
